94fc8c3a1ce6c9828dc82fbbdae6451b1dae545a
Littlejohn <Cdlittlejohn1@gmail.com>
2019-05-06 18:12:16 -0400

    Updated tests

--- Tests/Selenium.Tests.ps1
+++ Tests/Selenium.Tests.ps1
@@ -1,10 +1,39 @@
-Import-Module (Join-Path $PSScriptRoot "Selenium.psm1")
+Import-Module $PSScriptRoot\..\Selenium -Force
 
 Describe "Get-SeCookie" {
-    Context "Should get cookies from google" {
-        $Driver = Start-SeFirefox
-        Enter-SeUrl -Driver $Driver -Url "http://www.google.com"
+    BeforeAll {
+        $ffdriver = Start-SeFirefox
+        $chromedriver = Start-SeChrome -Arguments "headless"
+    }
 
-        Get-SeCookie $Driver
+    AfterAll {
+        Stop-SeDriver -Driver $ffdriver
+        Stop-SeDriver -Driver $chromedriver
+    }
+
+    Context "FireFox" {
+        It "Should get cookies from <URL>" -TestCases @(
+            @{URL = "https://www.nasa.gov/"}
+            #@{URL = "https://github.com/"}
+            @{URL = "https://www.netflix.com/"}
+        ) {
+            param($URL)
+
+            Enter-SeUrl -Driver $ffdriver -Url $URL
+            Get-SeCookie $ffdriver
+        }
+    }
+
+    Context "Chrome" {
+        It "Should get cookies from <URL>" -TestCases @(
+            @{URL = "https://www.nasa.gov/"}
+            @{URL = "https://github.com/"}
+            @{URL = "https://www.netflix.com/"}
+        ) {
+            param($URL)
+
+            Enter-SeUrl -Driver $chromedriver -Url $URL
+            Get-SeCookie $chromedriver
+        }
     }
 }
